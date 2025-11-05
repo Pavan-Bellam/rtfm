dd6c40c8629081ba8261a9e8b1dc5c890a011889
Pavan Bellam <pavan94411@gmail.com>
2025-11-05 08:04:46 -0500

    chore(alembic): import models and create migration files

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import engine_from_config, pool
 from alembic import context
 from app.core.db import Base
 from app.core.settings import settings
+from app.models import *
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
